591128d20d83e1547f83e185eea1227877b6a0e8
nam.vhh <namhoailu@gmail.com>
2023-03-28 22:40:32 +0700

    docs: api specs get location

---
+++ client_side/src/constants/constants_select.ts
@@ -0,0 +1,24 @@
+export const constantsSelect = {
+  DATA_PROVINCE: [
+    {
+      value: 'Ho Chi Minh',
+      label: 'Ho Chi Minh',
+      district: [
+        { value: 'District 1', label: 'District 1' },
+        { value: 'District 2', label: 'District 2' },
+        { value: 'District 3', label: 'District 3' },
+        { value: 'District 4', label: 'District 4' },
+        { value: 'District 5', label: 'District 5' },
+      ],
+    },
+    {
+      value: 'Ha Noi',
+      label: 'Ha Noi',
+      district: [
+        { value: 'Ho Tay', label: 'Ho Tay' },
+        { value: 'Ho Hoan Kiem', label: 'Ho Hoan Kiem' },
+        { value: 'Dong Da', label: 'Dong Da' },
+      ],
+    },
+  ],
+};
---
+++ client_side/src/pages/Login/components/RegisterCustomer/constants.ts
@@ -0,0 +1,11 @@
+export const keyRegisterCustomer = {
+  USERNAME: 'username',
+  FULL_NAME: 'fullName',
+  PASSWORD: 'password',
+  CONFIRM_PASSWORD: 'confirmPassword',
+  EMAIL: 'email',
+  PHONE: 'phone',
+  ADDRESS: 'address',
+  PROVINCE: 'province',
+  DISTRICT: 'district',
+};
