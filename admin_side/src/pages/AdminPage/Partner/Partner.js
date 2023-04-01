7a311e778f731e79c0bf1d4ba75ccacb3c13988c
Trung4123 <thieuvinhtrung4123@gmail.com>
2023-04-01 14:43:06 +0700

    delete useState data

--- admin_side/src/pages/AdminPage/Partner/Partner.js
+++ admin_side/src/pages/AdminPage/Partner/Partner.js
@@ -1,37 +1,29 @@
 import { React, useState, useEffect } from 'react';
-import { Table} from 'antd';
+import { Table } from 'antd';
 
 import './partner.scss';
 import { COLUMNS_PARTNER } from '../constColumn/const';
 
 
-
-
 function Partner() {
     const columns = COLUMNS_PARTNER;
-    const [dataSource, setDataSource] = useState([]);       // data source for table
-
-    // get data from server
-    useEffect(() => {
-        const data = [];
-        for (let i = 0; i < 5; i++) {
-            data.push({
-                key: `${i}`,
-                email: `Email ${i}`,
-                representative: `Representative ${i}`,
-                restaurant: `Restaurant ${i}`,
-                phone: `Phone ${i}`,
-                city: `City ${i}`,
-                bank: `Bank ${i}`,
-            })
-        }
-        setDataSource(data);
-    }, [])
+    const data = [];
+    for (let i = 0; i < 5; i++) {
+        data.push({
+            key: `${i}`,
+            email: `Email ${i}`,
+            representative: `Representative ${i}`,
+            restaurant: `Restaurant ${i}`,
+            phone: `Phone ${i}`,
+            city: `City ${i}`,
+            bank: `Bank ${i}`,
+        })
+    }
 
     return (
         <div>
             <h1 className="page_container_title"> Partner Page</h1>
-            <Table dataSource={dataSource} columns={columns} />
+            <Table dataSource={data} columns={columns} />
         </div>
     );
 }
--- admin_side/src/pages/AdminPage/Staff/Staff.js
+++ admin_side/src/pages/AdminPage/Staff/Staff.js
@@ -9,40 +9,15 @@ import { COLUMNS_STAFF } from '../constColumn/const';
 
 function Staff() {
     const columns = COLUMNS_STAFF;
-    const [dataSource, setDataSource] = useState([]);       // data source for table
-    const [lock, setLock] = useState(faLock);               // icon for block button
-    const [editInfo, setEditInfo] = useState(null);        // edit info for table
-    const [form] = Form.useForm();                         // form for table
-    const [editMode, setEditMode] = useState(false);       // hide/show edit button - save button
+    const data = [];
+    for (let i = 0; i < 5; i++) {
+        data.push({
+            key: `${i}`,
+            username: `Username ${i}`,
+            password: `Password ${i}`,
+        })
+    }
 
-
-
-    useEffect(() => {
-        const data = [];
-        for (let i = 0; i < 5; i++) {
-            data.push({
-                key: `${i}`,
-                username: `Username ${i}`,
-                password: `Password ${i}`,
-                
-            })
-        }
-        setDataSource(data);
-    }, [])
-
-    const handleFinish = (values) => {
-        const newData = [...dataSource];
-        newData.splice(editInfo, 1, { ...values, key: editInfo });
-        setDataSource(newData);
-        setEditInfo(null);
-    };
-
-    const handleDelete = (record) => {
-        const newData = [...dataSource];
-        const index = newData.findIndex((item) => record.key === item.key);
-        newData.splice(index, 1);
-        setDataSource(newData);
-      };
 
     return (
         <div>
@@ -51,9 +26,7 @@ function Staff() {
                 <FontAwesomeIcon icon={faPlus} />
                 Add Staff
             </Button>
-            <Form form={form} onFinish={handleFinish}>
-                <Table dataSource={dataSource} columns={columns} />
-            </Form>
+            <Table dataSource={data} columns={columns} />
         </div>
     );
 }
