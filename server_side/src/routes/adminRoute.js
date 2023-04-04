7af450d5d6f4121b08f21a16128d96eecb72f10c
nhman2002 <nhman2002@gmail.com>
2023-04-04 14:36:12 +0700

    getAllShipper

--- server_side/src/routes/adminRoute.js
+++ server_side/src/routes/adminRoute.js
@@ -5,8 +5,10 @@ const { authorizeUser } = require('../middlewares/auth');
 router.post('/register-staff', authorizeUser('admin'), controller.registerStaff);
 router.delete('/delete-staff/:id', authorizeUser('admin'), controller.deleteStaff);
 router.post('/update-accountStatus/:id', authorizeUser('admin'), controller.updateStatusAccount);
-router.post('/get-staff/:id', authorizeUser('admin'), controller.getStaff);
+router.get('/get-staff/:id', authorizeUser('admin'), controller.getStaff);
 router.get('/get-allStaff', authorizeUser('admin'), controller.getAllStaff);
+router.get('/get-allShipper', authorizeUser('admin'), controller.getAllShipper);
+
 
 
 
